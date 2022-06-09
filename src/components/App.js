ef87d824b89a6a8c9ea297e08881a87bc5bdc175
Suvro96 <uniquesuvro@gmail.com>
2022-06-09 14:46:02 +0530

    digital clock

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
+
 const App = () => {
+  let date = new Date().toLocaleDateString();
+  let time = new Date().toLocaleTimeString();
+  const [tym, update] = useState(time);
+
+  const runningTime = () => {
+    time = new Date().toLocaleTimeString();
+    update(time);
+  };
+  setInterval(runningTime, 1000);
+
+  useEffect(() => {
+    clearInterval(runningTime);
+  });
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{date + " , " + tym}</div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
